da43898e17f9de815ab1b1b3e136edccc13eee22
Chew Boon Heng <boonheng85@gmail.com>
2018-04-10 07:24:10 +0800

    created base role

---
+++ README.md
@@ -0,0 +1,13 @@
+# Local Kubernetes Setup using Ansible
+
+A set of ansible scripts and documentation to setup a cluster of Kubernetes (with HA) using kubeadm. 
+
+## Overview
+
+This repository compose of these playbooks:
+
+1. `setup_http_yum_repo.yml`
+
+## Prerequisites
+
+Ansible version 2.4.2 or higher is required. Older versions might not work. 
--- repo_download.sh
+++ repo_download.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-wget -P /binaries/epel-addon/7/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/h/htop-2.1.0-1.el7.x86_64.rpm
-wget -P /binaries/epel-addon/7/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/j/jq-1.5-1.el7.x86_64.rpm
-wget -P /binaries/epel-addon/7/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/x/xclip-0.12-5.el7.x86_64.rpm
+wget -P /binaries/epel-addon/7/x86_64/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/h/htop-2.1.0-1.el7.x86_64.rpm
+wget -P /binaries/epel-addon/7/x86_64/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/j/jq-1.5-1.el7.x86_64.rpm
+wget -P /binaries/epel-addon/7/x86_64/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/x/xclip-0.12-5.el7.x86_64.rpm
+wget -P /binaries/epel-addon/7/x86_64/Packages http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/o/oniguruma-5.9.5-3.el7.x86_64.rpm
 
 
